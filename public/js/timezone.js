0e680fba587aa10c161ce2e586d49e5ce4f88d03
Jared <91357852+jad7qt@users.noreply.github.com>
2025-10-08 23:11:21 -0400

    Update timezone js

--- public/js/timezone.js
+++ public/js/timezone.js
@@ -1,2 +1,10 @@
-const usertimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-console.log("User's timezone:", usertimezone);
+// Function to get user current timezone and send to PHP scripts
+document.addEventListener('DOMContentLoaded', () => {
+  const usertimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  console.log("User's timezone:", usertimezone);
+  fetch('save_timezone.php', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ timezone: usertimezone })
+  });
+});
